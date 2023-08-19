07cc7f075320cfa0e0466e148e94156fecb50de6
Alex Derbes <xor@bentboolean.com>
2023-08-18 20:23:15 -0700

    radio

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.Heart)
+    basic.showNumber(receivedNumber)
+    radio.sendNumber(0)
+    music.setVolume(51)
+    music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.InBackground)
+    pins.analogWritePin(AnalogPin.P12, 1023)
+    pins.analogWritePin(AnalogPin.P13, 0)
+})
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_EVT_ANY, function () {
     basic.showIcon(IconNames.No)
+    music.stopAllSounds()
     pins.analogWritePin(AnalogPin.P12, 0)
     pins.analogWritePin(AnalogPin.P13, 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, function () {
-    basic.showIcon(IconNames.Heart)
-    pins.analogWritePin(AnalogPin.P12, 1023)
-    pins.analogWritePin(AnalogPin.P13, 0)
+	
 })
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
